dff8e9887d2a035d38247d5d1eb10f7a8372684b
Kalynn Kosyka <KKOSYKA@kalynns-macbook-pro.local>
2015-12-31 15:45:13 -0500

    Initial Commit

---
+++ TIP$/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIP$
+//
+//  Created by Kalynn Kosyka on 12/31/15.
+//  Copyright © 2015 kkosyka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
